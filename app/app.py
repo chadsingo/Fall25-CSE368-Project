8a3b104beed5fef33af964b4bd56d73011c06add
codeinechad <143447937+codeinechad@users.noreply.github.com>
2025-11-13 11:49:58 -0500

    base generation route setup

--- app/app.py
+++ app/app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 
+
+from utils.generate_suggestion import generate_suggestion
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    
+
     return "<p>Hello, World!</p>"
 
 
 
 @app.route("/suggest")
 def suggest():
-    suggestion = ''
+    suggestion = generate_suggestion()
 
     return suggestion
