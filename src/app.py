a5edb22278fbe404d900307923062afad0f7d1e0
domom <68618855+domomTV@users.noreply.github.com>
2025-11-13 12:34:10 -0500

    gemini added
    - Gemini added for generate_suggestions
      - Note: .env file needed for gemini
    - app folder renamed to src

--- src/app.py
+++ src/app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-
-
 from utils.generate_suggestion import generate_suggestion
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
 
     return "<p>Hello, World!</p>"
-
 
 
 @app.route("/suggest")
